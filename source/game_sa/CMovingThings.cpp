b45793018f2c07afe5b541e4eb6d48a610819231
August <vlad.socolov@mail.ru>
2021-08-19 02:58:10 +0300

    Reordered functions

--- source/game_sa/CMovingThings.cpp
+++ source/game_sa/CMovingThings.cpp
@@ -1,26 +1,11 @@
 #include "StdInc.h"
 
 void CMovingThings::InjectHooks() {
-//    ReversibleHooks::Install("CMovingThings", "Init", 0x717D00, &CMovingThings::Init);
-//    ReversibleHooks::Install("CMovingThings", "Shutdown", 0x717C20, &CMovingThings::Shutdown);
-//    ReversibleHooks::Install("CMovingThings", "Update", 0x7185B0, &CMovingThings::Update);
 //    ReversibleHooks::Install("CMovingThings", "Render", 0x717150, &CMovingThings::Render);
 //    ReversibleHooks::Install("CMovingThings", "Render_BeforeClouds", 0x7178F0, &CMovingThings::Render_BeforeClouds);
-}
-
-// 0x717D00
-void CMovingThings::Init() {
-    plugin::Call<0x717D00>();
-}
-
-// 0x717C20
-void CMovingThings::Shutdown() {
-    plugin::Call<0x717C20>();
-}
-
-// 0x7185B0
-void CMovingThings::Update() {
-    plugin::Call<0x7185B0>();
+//    ReversibleHooks::Install("CMovingThings", "Shutdown", 0x717C20, &CMovingThings::Shutdown);
+//    ReversibleHooks::Install("CMovingThings", "Init", 0x717D00, &CMovingThings::Init);
+//    ReversibleHooks::Install("CMovingThings", "Update", 0x7185B0, &CMovingThings::Update);
 }
 
 // 0x717150
@@ -32,3 +17,19 @@ void CMovingThings::Render() {
 void CMovingThings::Render_BeforeClouds() {
     plugin::Call<0x7178F0>();
 }
+
+// 0x717C20
+void CMovingThings::Shutdown() {
+    plugin::Call<0x717C20>();
+}
+
+// 0x717D00
+void CMovingThings::Init() {
+    plugin::Call<0x717D00>();
+}
+
+// 0x7185B0
+void CMovingThings::Update() {
+    plugin::Call<0x7185B0>();
+}
+
--- source/game_sa/CMovingThings.h
+++ source/game_sa/CMovingThings.h
@@ -4,9 +4,9 @@ class CMovingThings {
 public:
     static void InjectHooks();
 
-    static void Init();
-    static void Shutdown();
-    static void Update();
     static void Render();
     static void Render_BeforeClouds();
+    static void Shutdown();
+    static void Init();
+    static void Update();
 };
